d1344677708775799d766a344f613448c81ae100
Sergio García Heredia <sergio.gh14@gmail.com>
2023-02-16 23:35:00 +0100

    Implement time delay

--- Ecosystem.py
+++ Ecosystem.py
@@ -9,6 +9,7 @@ class Ecosystem:
         self.delay = delay
         self.network = network
         self.num_species = H.shape[0]
+        self._dead_nodes = []
 
     
     def random_init(self):
@@ -20,12 +21,16 @@ class Ecosystem:
 
 
     def evolve(self):
-        dead_node = rd.randint(0, self.network.num_nodes)
-        connected_nodes = np.argwhere(self.network.links[dead_node]).squeeze()
-        competing_nodes = rd.choice(connected_nodes, 2)
-        competing_nodes_classes = self.network.nodes[competing_nodes]
-        p = self.H[tuple(competing_nodes_classes - 1)]
-        winner_node = rd.choice(competing_nodes, p=np.array([p, 1 - p]))
-        self.network.nodes[dead_node] = self.network.nodes[winner_node]
+        new_dead_node = rd.randint(0, self.network.num_nodes)
+        self._dead_nodes.append(new_dead_node)
+        if self.time % self.delay == 0 and self.time > 0:
+            old_dead_node = self._dead_nodes.pop(0)
+            connected_nodes = np.argwhere(self.network.links[old_dead_node]).squeeze()
+            competing_nodes = rd.choice(connected_nodes, 2)
+            competing_nodes_classes = self.network.nodes[competing_nodes]
+            p = self.H[tuple(competing_nodes_classes - 1)]
+            winner_node = rd.choice(competing_nodes, p=np.array([p, 1 - p]))
+            self.network.nodes[old_dead_node] = self.network.nodes[winner_node]
+        
         self.time += 1
             
