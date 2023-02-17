658ab3d17db87ee914d7f15aa7749982cda4d5b3
Sergio García Heredia <sergio.gh14@gmail.com>
2023-02-17 12:37:28 +0100

    Implement `_death()` and `._reproduction()` methods

--- Ecosystem.py
+++ Ecosystem.py
@@ -19,17 +19,26 @@ class Ecosystem:
             size=self.network.num_nodes
         )
 
+    
+    def _death(self):
+        new_dead_node = rd.randint(0, self.network.num_nodes)
+        self.network.nodes[new_dead_node] = 0
+        self._dead_nodes.append(new_dead_node)
+
+    
+    def _reproduction(self):
+        old_dead_node = self._dead_nodes.pop(0)
+        connected_nodes = np.argwhere(self.network.links[old_dead_node]).squeeze()
+        competing_nodes = rd.choice(connected_nodes, 2)
+        competing_nodes_classes = self.network.nodes[competing_nodes]
+        p = self.H[tuple(competing_nodes_classes - 1)]
+        winner_node = rd.choice(competing_nodes, p=np.array([p, 1 - p]))
+        self.network.nodes[old_dead_node] = self.network.nodes[winner_node]
+
 
     def evolve(self):
         self.time += 1
-        new_dead_node = rd.randint(0, self.network.num_nodes)
-        self._dead_nodes.append(new_dead_node)
+        self._death()
         if self.time % (self.delay + 1) == 0:
-            old_dead_node = self._dead_nodes.pop(0)
-            connected_nodes = np.argwhere(self.network.links[old_dead_node]).squeeze()
-            competing_nodes = rd.choice(connected_nodes, 2)
-            competing_nodes_classes = self.network.nodes[competing_nodes]
-            p = self.H[tuple(competing_nodes_classes - 1)]
-            winner_node = rd.choice(competing_nodes, p=np.array([p, 1 - p]))
-            self.network.nodes[old_dead_node] = self.network.nodes[winner_node]
-        
+            self._reproduction()
+            
